84ebf6a953d9320f5362f9a66acd850b418ec6ab
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-10-14 13:19:54 +0200

    adding an uber logo

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import React from 'react';
 import tailwind from 'twrnc';
 const HomeScreen = () => {
   return (
     <View style={tailwind`bg-white h-full`}>
-     <View>UBER</View>
+      <View>
+        <Image
+          source={{
+            uri: 'https://download.logo.wine/logo/Uber/Uber-Logo.wine.png',
+          }}
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+        ></Image>
+      </View>
     </View>
   );
 };
